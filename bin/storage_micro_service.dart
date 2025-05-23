ce06c769d7f115f276229e9b8ce79cd445f580f7
amrmhassan <amrmhassanmarsafa@gmail.com>
2025-05-23 03:44:59 +0300

    a

--- bin/storage_micro_service.dart
+++ bin/storage_micro_service.dart
@@ -4,14 +4,11 @@ import 'package:dart_flux/dart_flux.dart';
 import 'package:storage_micro_service/handler.dart';
 
 void main(List<String> arguments) async {
-  Router router = Router()
-      .handler(AppHandlers.uploadHandler)
-      .handler(AppHandlers.downloadHandler);
-  Server server = Server(
-    InternetAddress.anyIPv4,
-    6000,
-    router,
-    upperMiddlewares: [Middleware(null, null, corsByPassing)],
-  );
+  Router router =
+      Router()
+        ..middleware(corsByPassing)
+            .handler(AppHandlers.uploadHandler)
+            .handler(AppHandlers.downloadHandler);
+  Server server = Server(InternetAddress.anyIPv4, 6000, router);
   await server.run();
 }
